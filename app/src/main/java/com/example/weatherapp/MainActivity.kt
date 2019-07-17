bad59482c725c66b0e4763ba1a58930bff12c7fb
Akash Raja <akash.raja@signify.com>
2019-07-17 19:40:02 +0530

    initializeNavHostFragment only if savedInstanceState is null

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -9,7 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initializeNavHostFragment();
+
+        if (savedInstanceState == null) {
+            initializeNavHostFragment();
+        }
     }
 
     private fun initializeNavHostFragment() {
